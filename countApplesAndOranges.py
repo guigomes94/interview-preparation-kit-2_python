ccccef2902d811a941ec8a932fc5ba3a4d232ed2
Guilherm Gomes <46453969+guigomes94@users.noreply.github.com>
2019-05-01 08:38:04 -0300

    Delete countApplesAndOranges.py

--- countApplesAndOranges.py
+++
@@ -1,7 +0,0 @@
-def countApplesAndOranges(s, t, a, b, apples, oranges):
-    res_apple = list(map(lambda v: v + a, apples))
-    res_orange = list(map(lambda v: v + b, oranges))
-    res_apple = list(filter(lambda v: v >= s and v <=t, res_apple))
-    res_orange = list(filter(lambda v: v >= s and v <=t, res_orange))
-    res = [len(res_apple), len(res_orange)]
-    return res
